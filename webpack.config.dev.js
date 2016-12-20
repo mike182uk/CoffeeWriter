dc47d9f3ea6bd0131134189bebe4ed5b5856b9cf
Michael Barrett <mike182uk@gmail.com>
2016-12-20 10:44:15 +0000

    update webpack configs

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,40 +1,6 @@
+const commonConfig = require('./webpack.config.common')
+const deepAssign = require('deep-assign')
 
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
-
-module.exports = {
-  devtool: 'sourcemaps',
-  entry: [
-    path.resolve(__dirname, 'src/app/index.js')
-  ],
-  output: {
-    pathinfo: true,
-    filename: 'bundle.js'
-  },
-  resolve: {
-    alias: {
-      'coffee-script': path.resolve(__dirname, 'vendor/coffee-script-1.10.0.min.js')
-    }
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        include: path.join(__dirname, 'src', 'app'),
-        loader: 'babel'
-      },
-      {
-        test: /\.css$/,
-        loaders: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/app/index.html')
-    })
-  ]
-}
+module.exports = deepAssign({}, commonConfig, {
+  devtool: 'sourcemaps'
+})
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,39 +1,4 @@
+const commonConfig = require('./webpack.config.common')
+const deepAssign = require('deep-assign')
 
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
-
-module.exports = {
-  entry: [
-    path.resolve(__dirname, 'src/app/index.js')
-  ],
-  output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'build', 'app')
-  },
-  resolve: {
-    alias: {
-      'coffee-script': path.resolve(__dirname, 'vendor/coffee-script-1.10.0.min.js')
-    }
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        include: path.join(__dirname, 'src', 'app'),
-        loader: 'babel'
-      },
-      {
-        test: /\.css$/,
-        loaders: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/app/index.html')
-    })
-  ]
-}
+module.exports = deepAssign({}, commonConfig, {})
