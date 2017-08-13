530968ac73b7648e19ce76d8c0d2a7edb5dd23f4
Michael Barrett <mike182uk@gmail.com>
2017-08-13 10:12:56 +0100

    clear input on focus if default value

--- src/app/containers/Editor/index.js
+++ src/app/containers/Editor/index.js
@@ -6,19 +6,57 @@ import 'codemirror/mode/coffeescript/coffeescript'
 
 import './styles.css'
 
-const Editor = ({ dispatch }) => {
-  let options = {
-    lineNumbers: true,
-    mode: 'coffeescript'
+class Editor extends React.Component {
+  get options () {
+    return {
+      lineNumbers: true,
+      mode: 'coffeescript'
+    }
   }
 
-  return (
-    <div className='Editor'>
-      <Codemirror options={options} onChange={input => {
-        dispatch(compileInput(input))
-      }} value='# CoffeeScript goes here...' />
-    </div>
-  )
+  get defaultValue () {
+    return '# CoffeeScript goes here...'
+  }
+
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      value: this.defaultValue
+    }
+
+    this.handleFocusChange = this.handleFocusChange.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+
+  updateValue (newValue) {
+    this.setState({
+      value: newValue
+    })
+  }
+
+  handleInputChange (input) {
+    this.updateValue(input)
+    this.props.dispatch(compileInput(input))
+  }
+
+  handleFocusChange (focused) {
+    if (focused && this.state.value === this.defaultValue) {
+      this.updateValue('')
+    }
+
+    if (!focused && this.state.value === '') {
+      this.updateValue(this.defaultValue)
+    }
+  }
+
+  render () {
+    return (
+      <div className='Editor'>
+        <Codemirror options={this.options} onChange={this.handleInputChange} onFocusChange={this.handleFocusChange} value={this.state.value} />
+      </div>
+    )
+  }
 }
 
 export default connect()(Editor)
